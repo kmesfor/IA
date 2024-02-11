6599cb9f5457145744797bfeefe9f8050822527d
kmesfor <74278881+kmesfor@users.noreply.github.com>
2024-02-10 22:39:07 -0500

    remove redundancies

--- src/com/gmail/kianmesforush/tutormanagement/Screen.java
+++ src/com/gmail/kianmesforush/tutormanagement/Screen.java
@@ -1,11 +1,10 @@
 package com.gmail.kianmesforush.tutormanagement;
 
 import javax.swing.*;
-import java.awt.*;
 
 public interface Screen {
 	//The JPanel within the main JFrame of the app
-	public JPanel panel = new JPanel();
+	JPanel panel = new JPanel();
 	//Method to load the panel's content, returns the panel
-	public JComponent show();
+	JComponent show();
 }
