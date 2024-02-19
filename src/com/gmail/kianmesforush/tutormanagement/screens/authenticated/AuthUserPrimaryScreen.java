26707357698d4e7526421ee573123efcf8851040
kmesfor <74278881+kmesfor@users.noreply.github.com>
2024-02-19 17:34:52 -0500

    Add buttons to AuthUserPrimaryScreen

--- src/com/gmail/kianmesforush/tutormanagement/screens/authenticated/AuthUserPrimaryScreen.java
+++ src/com/gmail/kianmesforush/tutormanagement/screens/authenticated/AuthUserPrimaryScreen.java
@@ -3,12 +3,25 @@ package com.gmail.kianmesforush.tutormanagement.screens.authenticated;
 import com.gmail.kianmesforush.tutormanagement.Screen;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class AuthUserPrimaryScreen implements Screen {
-	private final JLabel messageLabel = new JLabel("Click on a button");
+	private final JButton tutorMgmtBtn = new JButton("Tutor Management");
+	private final JButton tuteeMgmtBtn = new JButton("Tutee Management");
+	private final JButton generalMgmtBtn = new JButton("General Management");
+	private final JButton createAptBtn = new JButton("Create Appointment");
+	private final JButton viewHoursBtn = new JButton("View completed tutoring hours");
+	private final JButton backBtn = new JButton("Back");
 	
 	public JComponent show() {
-		panel.add(messageLabel);
+		//TODO: layout
+		panel.add(tutorMgmtBtn);
+		panel.add(tuteeMgmtBtn);
+		panel.add(generalMgmtBtn);
+		panel.add(createAptBtn);
+		panel.add(viewHoursBtn);
+		panel.add(backBtn);
+		
 		return panel;
 	}
 }
