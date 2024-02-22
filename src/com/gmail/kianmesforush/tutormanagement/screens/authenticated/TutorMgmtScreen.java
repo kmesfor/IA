493ad0bfdfd34e1fe11c0915e2129e0e35598f85
kmesfor <74278881+kmesfor@users.noreply.github.com>
2024-02-21 23:59:55 -0500

    Create TutorMgmtScreen.java

---
+++ src/com/gmail/kianmesforush/tutormanagement/screens/authenticated/TutorMgmtScreen.java
@@ -0,0 +1,14 @@
+package com.gmail.kianmesforush.tutormanagement.screens.authenticated;
+
+import com.gmail.kianmesforush.tutormanagement.data.Screen;
+
+import javax.swing.*;
+
+public class TutorMgmtScreen implements Screen {
+	
+	public JComponent show() {
+		
+		return panel;
+	}
+}
+
