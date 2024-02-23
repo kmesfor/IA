711d430f34cbed253f9f74f8b69b19a4803cb982
kmesfor <74278881+kmesfor@users.noreply.github.com>
2024-02-23 00:19:34 -0500

    Create data manager and serialization

---
+++ src/com/gmail/kianmesforush/tutormanagement/DataManager.java
@@ -0,0 +1,74 @@
+package com.gmail.kianmesforush.tutormanagement;
+
+import com.gmail.kianmesforush.tutormanagement.datatypes.TutoringSession;
+import com.gmail.kianmesforush.tutormanagement.screens.ErrorScreen;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//Serialization method via https://www.tutorialspoint.com/java/java_serialization.htm
+public class DataManager {
+	public static ArrayList<TutoringSession> sessions = new ArrayList<>();
+	
+	//Compiler causing unnecessary errors, DataManager is the only class that will touch the data files
+	@SuppressWarnings("unchecked")
+	public static void initialize() {
+		sessions = (ArrayList<TutoringSession>) deserialize("data/sessions");
+	}
+	
+	public static void save() {
+		serialize(sessions, "data/sessions");
+	}
+
+
+	private static void serialize(Object object, String path) {
+		try {
+			FileOutputStream fileStream = new FileOutputStream(path);
+			ObjectOutputStream objectStream = new ObjectOutputStream(fileStream);
+			objectStream.writeObject(object);
+			objectStream.close();
+			fileStream.close();
+		} catch (Exception err) {
+			// Also print message as it may be present during an exit-save
+			System.out.println(err.getMessage());
+			ScreenManager.setCurrentScreen(new ErrorScreen("Error serializing data: " + err.getMessage()));
+		}
+	}
+	
+	// https://www.baeldung.com/java-how-to-create-a-file
+	private static Object deserialize(String path) {
+		Object returnValue;
+		try {
+			//Initiate file and object streams
+			File file = new File(path);
+			FileInputStream fileStream = new FileInputStream(path);
+			ObjectInputStream objectStream = new ObjectInputStream(fileStream);
+			// read the file
+			returnValue = objectStream.readObject();
+			//close streams
+			fileStream.close();
+			objectStream.close();
+		} catch (FileNotFoundException err) {
+			//Try to create the file if it is not found
+			File file = new File(path);
+			try {
+				//Create file, show error if the file could not be created
+				if (!file.createNewFile()) ScreenManager.setCurrentScreen(new ErrorScreen("Error creating data file: " + err.getMessage()));
+			} catch (IOException fileError) {
+				//Show any other type of error
+				ScreenManager.setCurrentScreen(new ErrorScreen("Error creating data file: " + err.getMessage()));
+			}
+			//return an empty value because the file was just created
+			return new ArrayList<>();
+			
+		} catch (IOException | ClassNotFoundException err) {
+			//file is empty or other error
+			return new ArrayList<>();
+		}
+		//return the read value
+		return returnValue;
+	}
+}
--- src/com/gmail/kianmesforush/tutormanagement/TutorManagement.java
+++ src/com/gmail/kianmesforush/tutormanagement/TutorManagement.java
@@ -1,10 +1,20 @@
 package com.gmail.kianmesforush.tutormanagement;
 
+import com.gmail.kianmesforush.tutormanagement.datatypes.TutoringSession;
 import com.gmail.kianmesforush.tutormanagement.screens.HomeScreen;
 
 public class TutorManagement {
 	public static void main(String[] args) {
 		//Initialize the ScreenManager which will drive the main function of the app
 		ScreenManager.initialize(new HomeScreen());
+		
+		DataManager.initialize();
+		DataManager.sessions.add(new TutoringSession("test"));
+	}
+	
+	public static void exit() {
+		//TODO:
+		DataManager.save();
+		System.exit(0);
 	}
 }
