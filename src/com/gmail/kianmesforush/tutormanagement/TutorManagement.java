5ec4fe10c9e40d6a4f8f61e015c08db2ef0e0b71
kmesfor <74278881+kmesfor@users.noreply.github.com>
2024-02-26 16:43:32 -0500

    Make TutorManagement global variables final

--- src/com/gmail/kianmesforush/tutormanagement/TutorManagement.java
+++ src/com/gmail/kianmesforush/tutormanagement/TutorManagement.java
@@ -1,20 +1,17 @@
 package com.gmail.kianmesforush.tutormanagement;
 
-import com.gmail.kianmesforush.tutormanagement.datatypes.Tutor;
-import com.gmail.kianmesforush.tutormanagement.datatypes.TutoringSession;
 import com.gmail.kianmesforush.tutormanagement.screens.HomeScreen;
-import com.gmail.kianmesforush.tutormanagement.screens.authenticated.TutorMgmtScreen;
 
 public class TutorManagement {
-	public static int SCREEN_WIDTH = 400;
-	public static int SCREEN_HEIGHT = 200;
-	public static int SCREEN_LOC_X = 300;
-	public static int SCREEN_LOC_Y = 300;
+	public static final int SCREEN_WIDTH = 400;
+	public static final int SCREEN_HEIGHT = 200;
+	public static final int SCREEN_LOC_X = 300;
+	public static final int SCREEN_LOC_Y = 300;
 	
-	public static int POPOUT_WIDTH = 200;
-	public static int POPOUT_HEIGHT = 200;
-	public static int POPOUT_LOC_X = 200;
-	public static int POPOUT_LOC_Y = 200;
+	public static final int POPOUT_WIDTH = 200;
+	public static final int POPOUT_HEIGHT = 200;
+	public static final int POPOUT_LOC_X = 200;
+	public static final int POPOUT_LOC_Y = 200;
 	
 	
 	
