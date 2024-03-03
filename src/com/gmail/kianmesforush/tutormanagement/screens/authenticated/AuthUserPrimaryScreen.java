4431cd743d0dedba8f5cca8410890c0756dc048c
kmesfor <74278881+kmesfor@users.noreply.github.com>
2024-03-03 18:43:46 -0500

    Update AuthUserPrimaryScreen to use new UserMgmtScreen

--- src/com/gmail/kianmesforush/tutormanagement/screens/authenticated/AuthUserPrimaryScreen.java
+++ src/com/gmail/kianmesforush/tutormanagement/screens/authenticated/AuthUserPrimaryScreen.java
@@ -3,6 +3,7 @@ package com.gmail.kianmesforush.tutormanagement.screens.authenticated;
 import com.gmail.kianmesforush.tutormanagement.DataManager;
 import com.gmail.kianmesforush.tutormanagement.datatypes.Screen;
 import com.gmail.kianmesforush.tutormanagement.ScreenManager;
+import com.gmail.kianmesforush.tutormanagement.datatypes.UserType;
 import com.gmail.kianmesforush.tutormanagement.screens.HomeScreen;
 
 import javax.swing.*;
@@ -40,11 +41,11 @@ public class AuthUserPrimaryScreen extends Screen {
 	private static class TutorMgmtBtnPressed implements ActionListener {
 		// Create a clone of DataManager.tutors so the saved data is not edited until a save button is pressed
 		// https://stackoverflow.com/questions/30074736/how-to-duplicate-an-array-list-in-java
-		public void actionPerformed(ActionEvent e) { ScreenManager.setCurrentScreen(new TutorMgmtScreen(new ArrayList<>(DataManager.tutors))); }
+		public void actionPerformed(ActionEvent e) { ScreenManager.setCurrentScreen(new UserMgmtScreen(new ArrayList<>(DataManager.tutors), UserType.TUTOR)); }
 	}
 	
 	private static class TuteeMgmtBtnPressed implements ActionListener {
-		public void actionPerformed(ActionEvent e) { ScreenManager.setCurrentScreen(new TuteeMgmtScreen()); }
+		public void actionPerformed(ActionEvent e) { ScreenManager.setCurrentScreen(new UserMgmtScreen(new ArrayList<>(DataManager.tutees), UserType.TUTEE)); }
 	}
 	
 	private static class GeneralMgmtButtonPressed implements ActionListener {
