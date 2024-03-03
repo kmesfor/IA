27277314979ca3de2d0b9f7e29c1ac8f69b976d7
kmesfor <74278881+kmesfor@users.noreply.github.com>
2024-03-03 18:41:18 -0500

    Remove Tutor and Tutee in favor of User with UserType field

--- src/com/gmail/kianmesforush/tutormanagement/datatypes/User.java
+++ src/com/gmail/kianmesforush/tutormanagement/datatypes/User.java
@@ -4,20 +4,28 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public abstract class User implements Serializable {
+public class User implements Serializable {
 	//https://www.tutorialspoint.com/java/java_serialization.htm
 	@Serial
 	private static final long serialVersionUID = 1L;
 	private final String name;
+	private final UserType type;
 	public final ArrayList<TutoringSession> availability = new ArrayList<>();
 	public final ArrayList<ClassName> classNames = new ArrayList<>();
 	public final ArrayList<Skill> skills = new ArrayList<>();
 	public final ArrayList<Proficiency> proficiencies = new ArrayList<>();
 	public final ArrayList<String> notes = new ArrayList<>();
 	
-	public User(String name) {
+	public User(String name, UserType type) {
 		this.name = name;
+		this.type = type;
 	}
 	
-	public String getName() { return this.name; }
+	public String getName() {
+		return this.name;
+	}
+	
+	public UserType getType() {
+		return type;
+	}
 }
