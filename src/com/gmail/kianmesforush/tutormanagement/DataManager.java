85e2102c8062955d859e71e73b66be114769522c
kmesfor <74278881+kmesfor@users.noreply.github.com>
2024-03-10 17:52:07 -0400

    Add Javadocs to DataManager

--- src/com/gmail/kianmesforush/tutormanagement/DataManager.java
+++ src/com/gmail/kianmesforush/tutormanagement/DataManager.java
@@ -6,7 +6,6 @@ import com.gmail.kianmesforush.tutormanagement.screens.ErrorScreen;
 import java.io.*;
 import java.util.ArrayList;
 
-//Serialization method via https://www.tutorialspoint.com/java/java_serialization.htm
 public class DataManager {
 	public static ArrayList<TutoringSession> sessions = new ArrayList<>();
 	public static ArrayList<ClassName> classNames = new ArrayList<>();
@@ -15,7 +14,11 @@ public class DataManager {
 	public static ArrayList<User> tutors = new ArrayList<>();
 	public static ArrayList<User> tutees = new ArrayList<>();
 	
-	//Compiler causing unnecessary errors, DataManager is the only class that will touch the data files
+	/**
+	 * Initialize the DataManager by deserializing field data located in the data/ directory
+	 */
+	//Suppress an unchecked cast warning, DataManager is the only class that will touch the data files,
+	//thus the cast is safe as long as serialization and deserialization use the same type
 	@SuppressWarnings("unchecked")
 	public static void initialize() {
 		sessions = (ArrayList<TutoringSession>) deserialize("data/sessions");
@@ -26,6 +29,9 @@ public class DataManager {
 		tutees = (ArrayList<User>) deserialize("data/tutees");
 	}
 	
+	/**
+	 * Save field data to external files located in the data/ directory
+	 */
 	public static void save() {
 		serialize(sessions, "data/sessions");
 		serialize(classNames, "data/classes");
@@ -34,22 +40,37 @@ public class DataManager {
 		serialize(tutors, "data/tutors");
 		serialize(tutees, "data/tutees");
 	}
-
-
+	
+	/**
+	 * Serialize an object and save to an external file.
+	 * @param object the object to be serialized
+	 * @param path filepath to save the object
+	 */
 	private static void serialize(Object object, String path) {
 		try {
+			//Create a new output stream to write to files
 			FileOutputStream fileStream = new FileOutputStream(path);
+			//Create an output stream specifically for writing objects
 			ObjectOutputStream objectStream = new ObjectOutputStream(fileStream);
+			//Write the object
 			objectStream.writeObject(object);
+			//Close the file streams
 			objectStream.close();
 			fileStream.close();
 		} catch (Exception err) {
-			// Also print message as it may be present during an exit-save
-			System.out.println(err.getMessage());
+			//Display an error screen that prevents the user from interacting until
+			// the error is resolved and the program is restarted
 			ScreenManager.setCurrentScreen(new ErrorScreen("Error serializing data: " + err.getMessage()));
+			// Also print message as the error may have occurred during an exit-save
+			System.out.println(err.getMessage());
 		}
 	}
 	
+	/**
+	 * Deserialize an object by reading serialized data from an external file.
+	 * @param path the file path of the file to deserialize
+	 * @return An object that should be cast to the type that was initially serialized
+	 */
 	// https://www.baeldung.com/java-how-to-create-a-file
 	private static Object deserialize(String path) {
 		Object returnValue;
