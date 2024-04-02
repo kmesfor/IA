868c48dc74b1826af0cc5a2fa4a5b0ba3d7e3918
kmesfor <74278881+kmesfor@users.noreply.github.com>
2024-04-02 11:41:14 -0400

    Create ViewHoursScreen

--- src/com/gmail/kianmesforush/tutormanagement/screens/authenticated/ViewHoursScreen.java
+++ src/com/gmail/kianmesforush/tutormanagement/screens/authenticated/ViewHoursScreen.java
@@ -1,9 +1,12 @@
 package com.gmail.kianmesforush.tutormanagement.screens.authenticated;
 
+import com.gmail.kianmesforush.tutormanagement.DataManager;
 import com.gmail.kianmesforush.tutormanagement.ScreenManager;
 import com.gmail.kianmesforush.tutormanagement.datatypes.Screen;
+import com.gmail.kianmesforush.tutormanagement.datatypes.User;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,8 +15,22 @@ public class ViewHoursScreen extends Screen {
 	private final JButton backBtn = new JButton("Back");
 	
 	public JComponent show(JPanel panel) {
+		panel.setLayout(new BorderLayout());
+		JPanel upperPanel = new JPanel(new GridLayout(0, 2));
 		backBtn.addActionListener(new BackBtnPressed());
-		panel.add(backBtn);
+		panel.add(backBtn, BorderLayout.SOUTH);
+		
+		for (User tutor: DataManager.tutors) {
+			upperPanel.add(new JLabel(tutor.getName()));
+			upperPanel.add(new JLabel(tutor.getHoursCompleted() + " hours tutored"));
+		}
+		
+		for (User tutee : DataManager.tutees) {
+			upperPanel.add(new JLabel(tutee.getName()));
+			upperPanel.add(new JLabel(tutee.getHoursCompleted() + " hours of tutoring received"));
+		}
+		
+		panel.add(upperPanel, BorderLayout.CENTER);
 		
 		return panel;
 	}
