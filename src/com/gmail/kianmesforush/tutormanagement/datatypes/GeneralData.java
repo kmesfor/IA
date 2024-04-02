1eaf64ecdaeccc7a120291d81e0a21dc18f601f6
kmesfor <74278881+kmesfor@users.noreply.github.com>
2024-04-02 11:42:20 -0400

    Add clean destroy method for GeneralData

--- src/com/gmail/kianmesforush/tutormanagement/datatypes/GeneralData.java
+++ src/com/gmail/kianmesforush/tutormanagement/datatypes/GeneralData.java
@@ -1,7 +1,10 @@
 package com.gmail.kianmesforush.tutormanagement.datatypes;
 
+import com.gmail.kianmesforush.tutormanagement.DataManager;
+
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class GeneralData implements Serializable {
@@ -21,6 +24,44 @@ public class GeneralData implements Serializable {
 	}
 	public String getUUID() {
 		return uniqueID;
+	}
+	
+	public void destroy() {
+		if (type == GeneralDataType.CLASS) {
+			DataManager.classNames.remove(this);
+			for (User user : DataManager.tutors) {
+				user.classNames.removeIf(data -> data.getUUID().equals(this.getUUID()));
+			}
+			for (User user : DataManager.tutees) {
+				user.classNames.removeIf(data -> data.getUUID().equals(this.getUUID()));
+			}
+		} else if (type == GeneralDataType.SESSION) {
+			DataManager.sessions.remove(this);
+			for (User user : DataManager.tutors) {
+				user.availability.removeIf(data -> data.getUUID().equals(this.getUUID()));
+			}
+			for (User user : DataManager.tutees) {
+				user.availability.removeIf(data -> data.getUUID().equals(this.getUUID()));
+			}
+		} else if (type == GeneralDataType.SKILL) {
+			DataManager.skills.remove(this);
+			for (User user : DataManager.tutors) {
+				user.skills.removeIf(data -> data.getUUID().equals(this.getUUID()));
+			}
+			for (User user : DataManager.tutees) {
+				user.skills.removeIf(data -> data.getUUID().equals(this.getUUID()));
+			}
+		} else if (type == GeneralDataType.PROFICIENCY) {
+			DataManager.proficiencies.remove(this);
+			for (User user : DataManager.tutors) {
+				user.proficiencies.removeIf(data -> data.getUUID().equals(this.getUUID()));
+			}
+			for (User user : DataManager.tutees) {
+				user.proficiencies.removeIf(data -> data.getUUID().equals(this.getUUID()));
+			}
+		} else {
+			System.out.println("Invalid GeneralData was attempted to be destroyed!");
+		}
 	}
 	
 	public GeneralData(String name, GeneralDataType type) {
