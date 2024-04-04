f4efe645cbbeace5c0a1282e1f45f460ce24023b
kmesfor <74278881+kmesfor@users.noreply.github.com>
2024-04-04 14:17:24 -0400

    Stylize

--- src/com/gmail/kianmesforush/tutormanagement/screens/authenticated/ManageAptScreen.java
+++ src/com/gmail/kianmesforush/tutormanagement/screens/authenticated/ManageAptScreen.java
@@ -2,6 +2,8 @@ package com.gmail.kianmesforush.tutormanagement.screens.authenticated;
 
 import com.gmail.kianmesforush.tutormanagement.DataManager;
 import com.gmail.kianmesforush.tutormanagement.ScreenManager;
+import com.gmail.kianmesforush.tutormanagement.StyleType;
+import com.gmail.kianmesforush.tutormanagement.StylingManager;
 import com.gmail.kianmesforush.tutormanagement.components.ManageAptComponent;
 import com.gmail.kianmesforush.tutormanagement.datatypes.Appointment;
 import com.gmail.kianmesforush.tutormanagement.datatypes.Screen;
@@ -19,19 +21,33 @@ public class ManageAptScreen extends Screen {
 	private final JButton createAptBtn = new JButton("Create appointment");
 	
 	public JComponent show(JPanel panel) {
+		panel.setLayout(new BorderLayout());
+		
 		backBtn.addActionListener(new BackBtnPressed());
 		createAptBtn.addActionListener(new CreateAptBtnPressed());
 		btnPanel.add(backBtn);
 		btnPanel.add(createAptBtn);
 		
-		aptsPanel.setLayout(new GridLayout(0, 1));
-		for (int i = 0; i < DataManager.appointments.size(); i++) {
-			aptsPanel.add((new ManageAptComponent(DataManager.appointments, i)).show(new JPanel()));
+		//Ensures there are at least 10 rows (styling)
+		aptsPanel.setLayout(new GridLayout(Math.max(DataManager.appointments.size(), 10), 1));
+		if (DataManager.appointments.isEmpty()) {
+			JLabel emptyLabel = new JLabel("No appointments created");
+			StylingManager.stylize(emptyLabel, StyleType.ERROR);
+			aptsPanel.add(emptyLabel);
+		} else {
+			for (int i = 0; i < DataManager.appointments.size(); i++) {
+				aptsPanel.add((new ManageAptComponent(DataManager.appointments, i)).show(new JPanel()));
+			}
+			
 		}
 		
-		panel.setLayout(new BorderLayout());
 		panel.add(btnPanel, BorderLayout.SOUTH);
 		panel.add(aptsPanel, BorderLayout.CENTER);
+		
+		StylingManager.stylize(backBtn, StyleType.SECONDARY);
+		StylingManager.stylize(createAptBtn, StyleType.PRIMARY);
+		StylingManager.stylize(panel, StyleType.PRIMARY);
+		
 		return panel;
 	}
 	
